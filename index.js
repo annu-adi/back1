5f802f3e1f61fbc0dd5d5606155443f40a3fd5e8
annu-adi <rashmi.maurya10@gmail.com>
2025-05-23 06:34:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const TodoModel = require('./models/Todo')
 const app = express()
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://front1-ivory.vercel.app"],
     methods:["GET","POST","PUT", "DELETE"],
     credentials:true
 }))
